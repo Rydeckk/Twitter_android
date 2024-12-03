fc9ec614d03fe9cfd65419f2405bc2b4671fc367
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2024-12-04 00:09:39 +0100

    set auth route to public

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthLoginDto, AuthRegisterDto } from './dto/auth.dto';
+import { Public } from './constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
+  @Public()
   @Post('register')
   async register(@Body() body: AuthRegisterDto) {
     return this.authService.register(body);
   }
 
+  @Public()
   @Post('login')
   async login(@Body() body: AuthLoginDto) {
     const { email, password } = body;
