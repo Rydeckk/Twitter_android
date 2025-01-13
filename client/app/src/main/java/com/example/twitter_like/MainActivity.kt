59d08771519c78eb02a00c736611080ed4fdd5d4
Rydeckk <romainguiffantwork@hotmail.com>
2025-01-13 10:41:23 +0100

    Add fragment conversation

--- client/app/src/main/java/com/example/twitter_like/MainActivity.kt
+++ client/app/src/main/java/com/example/twitter_like/MainActivity.kt
@@ -3,18 +3,41 @@ package com.example.twitter_like
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.twitter_like.viewmodel.MainViewModel
+import com.example.twitter_like.views.fragment.ConversationFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Initialisation du ViewModel
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+        // Observer le fragment courant à afficher
+        viewModel.selectedFragment.observe(this, { fragment ->
+            fragment?.let {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainer, it)
+                    .commit()
+            }
+        })
+
+        // Si aucune sauvegarde (premier lancement), définir un fragment par défaut
+        if (savedInstanceState == null) {
+            viewModel.setFragment(ConversationFragment()) // Définir le fragment initial
         }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, fragment)
+            .commit()
     }
 }
