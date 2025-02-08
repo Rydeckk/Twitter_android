8c26a0169a3124e10fae4e6e601dade12e4ff1be
Nicolas9000 <92756075+Nicolas9000@users.noreply.github.com>
2025-02-08 18:52:48 +0100

    refactor: use global model/mapper structure

--- client/app/src/main/java/com/example/twitter_like/network/dto/conversation_dto/ConversationDto.kt
+++ client/app/src/main/java/com/example/twitter_like/network/dto/conversation_dto/ConversationDto.kt
@@ -1,6 +1,6 @@
-package com.example.twitter_like.models
+package com.example.twitter_like.network.dto.conversation_dto
 
-data class ConversationModel(
+data class ConversationDto(
     val id: Int,
     val title: String,
     val lastMessage: String,
