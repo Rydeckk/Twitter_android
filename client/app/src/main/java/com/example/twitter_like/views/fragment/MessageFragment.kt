1796a9c7a681fe2967bdeb811cfde42eaacf6fcc
Rydeckk <romainguiffantwork@hotmail.com>
2025-02-21 16:56:56 +0100

    Add retrofit client

--- client/app/src/main/java/com/example/twitter_like/views/fragment/MessageFragment.kt
+++
@@ -1,8 +0,0 @@
-package com.example.twitter_like.views.fragment
-
-import androidx.fragment.app.Fragment
-import com.example.twitter_like.R
-
-class MessageFragment: Fragment(R.layout.message_fragment) {
-
-}
